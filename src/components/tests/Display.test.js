a13f9553a62fc5998f30ec89ecfda35d3124ea50
Winifred Lu <winnie.lu@comcast.net>
2022-01-24 16:20:51 -0500

    tried show component test, array of seasons needs debugging

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,12 +4,38 @@ import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
-
+import fetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
 test('renders without errors with no props', ()=>{
     render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async()=>{
+    fetchShow.mockResolvedValueOnce({
+        data: [{name: 'testData',
+        image: null,
+        summary: 'testSummary',
+        seasons: [{
+            id: 1,
+            name: 'testSeason1',
+            episodes: []
+            }, {
+            id: 2,
+            name: 'testSeason2',
+            episodes: []
+            }, {
+            id: 3,
+            name: 'testSeason3',
+            episodes: []
+            }]
+        }]
+    });
+    render(<Display displayFunc={true}/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const selectOptions = await screen.queryAllByTestId('season-option');
+    expect(selectOptions).toHaveLength(3);
+});
 
 test('renders show season options matching your data when the button is clicked', ()=>{});
